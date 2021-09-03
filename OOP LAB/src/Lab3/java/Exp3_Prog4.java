9a9b44af2c60baadc0af3a3ae66184031a4b4cba
Akhand Pratap Singh <72747530+Akhand6886@users.noreply.github.com>
2021-09-03 12:29:36 +0530

    Edited code
    Added else condition

--- OOP LAB/src/Lab3/java/Exp3_Prog4.java
+++ OOP LAB/src/Lab3/java/Exp3_Prog4.java
@@ -1,13 +1,9 @@
 package Lab3.java;
 
 public class Exp3_Prog4 {
-
 	public static void main(String[] args) {
-
 	        int sum=0;
-	        
-	        for(int i=41;i<250;i++){
-	             
+	        for(int i=41;i<250;i++){   
 	            if(i%5!=0){
 	                sum=sum+i;   
 	            }
@@ -19,5 +15,4 @@ public class Exp3_Prog4 {
 	         
 	        System.out.println("The sum of intergers from 40 to 250 that divisible by 5 \n "+sum);
 	    }
-
 	}
